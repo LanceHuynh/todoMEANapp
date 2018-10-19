c70c97cf6eaad4ff88252012ba0955f8bb750c99
git <github.com>
2018-10-19 13:28:14 +0300

    bandage the code 'cause no skill D:

--- public/scripts/controllers/todo.js
+++ public/scripts/controllers/todo.js
@@ -6,7 +6,7 @@ angular.module('todoListApp')
     $scope.todos.splice(index, 1);
     dataService.deleteTodo(todo);
   };
-  
+
   $scope.saveTodos = function() {
     var filteredTodos = $scope.todos.filter(function(todo){
       if(todo.edited) {
@@ -14,5 +14,5 @@ angular.module('todoListApp')
       };
     })
     dataService.saveTodos(filteredTodos);
-  }; 
+  };
 });
--- src/app.js
+++ src/app.js
@@ -9,6 +9,6 @@ app.use('/', express.static('public'));
 
 app.use('/api', router);
 
-app.listen(process.env.PORT || 5000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log('App is running on port 3000');
 });
